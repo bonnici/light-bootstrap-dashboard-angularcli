f626fbfb9362ae6b306cc13c8a2702f78ad744f1
Bonnici <nickmarsh@gmail.com>
2016-12-19 20:45:16 +1000

    Copied over assets and installed dependencies

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  public title = 'app works!';
+
+  public ngOnInit(): void {
+    // demo.initChartist(); todo re-add when graph components are ready, move to afterviewinit? add typings, eventually re-write demo.js into angular style
+
+    $.notify({
+      icon: 'pe-7s-gift',
+      message: "Welcome to <b>Light Bootstrap Dashboard</b> - a beautiful freebie for every web developer."
+
+    },{
+      type: 'info',
+      timer: 4000
+    });
+  }
 }
--- src/typings.d.ts
+++ src/typings.d.ts
@@ -1,2 +1,6 @@
 // Typings reference file, you can add your own global typings here
 // https://www.typescriptlang.org/docs/handbook/writing-declaration-files.html
+
+interface JQueryStatic {
+  notify(options: any, settings: any);
+}
